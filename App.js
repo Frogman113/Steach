55fca421a68efbce702db43cef9f3fdb7e163ae3
Frogman113 <bainstud@naver.com>
2024-12-09 07:48:07 +0900

    Feat(타이틀): Steach 타이틀 글자 애니메이션 구현

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
 
 export default function App() {
+  const titleOpacity = useSharedValue(0);
+  const titleScale = useSharedValue(0.5);
+
+  const titleAnimationStyle = useAnimatedStyle(() => {
+    return {
+      opacity: titleOpacity.value,
+      transform: [{ scale: titleScale.value }],
+    };
+  });
+
+  useEffect(() => {
+    titleOpacity.value = withTiming(1, { duration: 2000 });
+    titleScale.value = withTiming(1, { duration: 2000 });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.Text style={[styles.titleText, titleAnimationStyle]}>
+        Steach
+      </Animated.Text>
     </View>
   );
 }
@@ -16,5 +37,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    color: '#3FC0D7',
+    fontSize: 80,
+    fontWeight: 'bold',
   },
 });
