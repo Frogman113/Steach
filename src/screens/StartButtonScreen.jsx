12594c003f8ea9ffee46de110b31c7082b704bb6
Frogman113 <bainstud@naver.com>
2024-12-12 04:49:30 +0900

    Feat(마이크 권한): 음성 녹음을 위해 마이크 권한이 없으면 요청

--- src/screens/StartButtonScreen.jsx
+++ src/screens/StartButtonScreen.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-import { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Audio } from 'expo-av';
 
 export default function StartButtonScreen() {
-  const [isRequire, setIsRequrie] = useState(false);
+  const startRecording = async () => {
+    try {
+      const permissionCheck = await Audio.requestPermissionsAsync();
+      if (permissionCheck.status !== 'granted') {
+        alert('마이크 권한이 필요합니다.');
+        return;
+      }
+
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+    } catch (error) {
+      alert('마이크 권한이 없습니다', error);
+    }
+  };
 
   return (
     <View style={styles.StartButtonContainer}>
-      <TouchableOpacity>
+      <TouchableOpacity onPress={startRecording}>
         <MaterialIcons name="multitrack-audio" size={100} color="black" />
       </TouchableOpacity>
     </View>
