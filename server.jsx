b3b45e3ebe97b3300ddc19da8205a6e4e0d50561
Frogman113 <bainstud@naver.com>
2024-12-13 07:44:06 +0900

    Feat(웹소켓 서버): Express 서버와 Websocket 연결

--- server.jsx
+++ server.jsx
@@ -1,13 +1,16 @@
 const express = require('express');
+const WebSocket = require('ws');
 const app = express();
 const port = 3000;
 
+const server = app.listen(port, () => {});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('close', () => {});
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('서버 작동 중');
 });
-
-app.post('/', (req, res) => {
-  res.send('Got a POST request');
-});
-
-app.listen(port, () => {});
