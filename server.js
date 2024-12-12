8304cbf3e2a09a80c5d47a942a03d39040241839
Frogman113 <bainstud@naver.com>
2024-12-13 08:24:51 +0900

    Feat(메세지 수신): 클라이언트로부터 웹소켓으로 메세지 수신 확인
    1. 텍스트를 받기위해 메시지 수신 여부 구현
    2. 클라이언트에서 데이터를 받고 다시 클라언트로 전송
    3. Node.js 환경에 적합하게 server.js로 파일명 변경

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const server = app.listen(port, () => {});
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
+  ws.on('message', (data) => {
+    ws.send(`데이터 보냄 ${data}`);
+  });
+
   ws.on('close', () => {});
 });
 
