3df38c26dd6e993f070d1a4725096f0c02aa2e15
Frogman113 <bainstud@naver.com>
2024-12-17 04:48:18 +0900

    Feat(웹소켓 연결): 음성을 텍스트로 변환할 때 웹소켓을 통해 변환하도록 구현
    1. 서버에서 HTTP 요청을 수행하기 위해 라이브러리 설치
    2. 서버에서 환경변수를 사용하기 위해 라이브러리 설치
    3. 후에 UI 변화 및 추가적인 기능을 위해 'close' 등 코드 배치

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const WebSocket = require('ws');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
 const app = express();
 const port = 3000;
 
@@ -8,8 +11,33 @@ const server = app.listen(port, () => {});
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    ws.send(`데이터 보냄 ${data}`);
+  ws.on('message', async (data) => {
+    try {
+      if (Buffer.isBuffer(data)) {
+        const clovaResponse = await fetch(
+          'https://clovaspeech-gw.ncloud.com/recog/v1/stt?lang=Kor',
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/octet-stream',
+              'X-CLOVASPEECH-API-KEY': process.env.CLOVA_SECRET_KEY,
+              'X-CLOVASPEECH-LANGUAGE': 'ko',
+            },
+            body: data,
+          },
+        );
+
+        const apiResult = await clovaResponse.json();
+
+        ws.send(JSON.stringify(apiResult));
+      }
+    } catch (error) {
+      ws.send(
+        JSON.stringify({
+          error: error.message,
+        }),
+      );
+    }
   });
 
   ws.on('close', () => {});
